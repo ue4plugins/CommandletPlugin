924be7b68822426d46ac7f2d331dd2c9607eed51
Max Preussner <Max.Preussner@epicgames.net>
2017-04-11 16:55:35 -0400

    Modernization pass (IWYU).

--- Source/CommandletPlugin/Private/CommandletPluginPrivate.h
+++ Source/CommandletPlugin/Private/CommandletPluginPrivate.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
+#include "Logging/LogMacros.h"
 
 
 /** Declares a log category for this module. */
--- Source/CommandletPlugin/Private/Commandlets/HelloWorldCommandlet.h
+++ Source/CommandletPlugin/Private/Commandlets/HelloWorldCommandlet.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "Commandlets/Commandlet.h"
+
 #include "HelloWorldCommandlet.generated.h"
 
 
